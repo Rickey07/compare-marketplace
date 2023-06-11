4b3f90a7a69ba8ed1a850f6b579e837376e3d9a3
Rickey07 <Rickeyrickey747@gmail.com>
2023-06-11 18:02:57 +0530

    SetUp Done

---
+++ Server/Models/user.ts
@@ -0,0 +1,34 @@
+import mongoose, { ObjectId } from "mongoose";
+import { v4 as uuidV4 } from "uuid";
+
+interface User {
+  name: string;
+  image_url: string;
+  password: string;
+  email: string;
+  objectIdField?: ObjectId;
+}
+
+const userSchema = new mongoose.Schema<User>({
+  name: {
+    type: String,
+    required: true,
+  },
+  image_url: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  objectIdField: { type: String },
+});
+
+
+const User = mongoose.model<User>('User',userSchema)
+
+export {User}
---
+++ Server/Utils/dbConnect.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectToDatabase = ():void => {
+    const dataBaseURL:string = process.env.DATABASE_URL ?? "null"
+    mongoose.connect(dataBaseURL,)
+    .then((data) => {
+        console.log(`MongoDB Connected with server: ${data.connection.host}`)
+    })
+    .catch((error) => {
+        console.log(error,"error is coming")
+    })
+}   
