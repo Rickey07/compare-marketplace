b6d7c983fa22ff7f8faaa4d69280d4c102bb7de0
Rickey07 <Rickeyrickey747@gmail.com>
2023-06-12 23:35:07 +0530

    Comments Added

--- Server/index.ts
+++ Server/index.ts
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
-
+// Connect to database
 connectToDatabase()
 
 app.listen(port,  () => {
